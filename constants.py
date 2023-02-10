0ea129955fd998379a767c164875f3169d919bbf
Carson <carsonburke22@gmail.com>
2023-02-10 09:33:06 -0800

    comments for constants

--- constants.py
+++ constants.py
@@ -1,7 +1,12 @@
 import string
 
+# How many password cracking simulations to run
 password_tests = 1000
+# The length of the password to guess for
 required_password_len = 2
-max_password_amount = 100000000000 # required_password_len * 10000
+# The max iterations a password crack attempt can take
+max_password_amount = 100000000000
+# Possible characters in a password
 password_options = string.ascii_letters + string.digits
+# Where to write the results to
 passwordsFilePath = 'passwords.txt'
